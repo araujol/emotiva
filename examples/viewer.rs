3940ba71ae276ff456323261219f9ea53c76b37e
Luis Araujo <fureai.studio@gmail.com>
2025-07-23 20:31:03 -0400

    refacotr(example): use new tween functions for the viewer example
    - Changed `viewer.rs` example to test only tween and eyes/mouth
      animation
    - Simplified its emotiva.ron file
    
    Signed-off-by: Luis Araujo <fureai.studio@gmail.com>

--- examples/viewer.rs
+++ examples/viewer.rs
@@ -2,22 +2,9 @@ use emotiva::quad::EmotivaQuad;
 
 use macroquad::prelude::*;
 
-fn move_char(emotiva: &mut EmotivaQuad) {
-    emotiva.trigger("base", "motion_play");
-    emotiva.trigger("eyes", "motion_play");
-    emotiva.trigger("mouth", "motion_play");
-}
-
-fn reverse_char(emotiva: &mut EmotivaQuad) {
-    emotiva.trigger("base", "motion_reverse");
-    emotiva.trigger("eyes", "motion_reverse");
-    emotiva.trigger("mouth", "motion_reverse");
-}
-
 #[macroquad::main("Emotiva Viewer")]
 async fn main() {
     let mut emotiva = EmotivaQuad::load("test_data/example.emotiva.ron", "test_data").await;
-    emotiva.set_base_position(Vec2::new(0.0, 0.0));
 
     let mut elapsed = 0.0;
     let mut toggled = true;
@@ -28,25 +15,20 @@ async fn main() {
         let delta = get_frame_time();
 
         elapsed += delta;
-        if elapsed >= 5.0 {
+        if elapsed >= 10.0 {
             if toggled {
                 println!("start!");
                 emotiva.trigger("eyes", "start_blinking");
-                emotiva.trigger("eyes", "tween_start");
                 emotiva.trigger("mouth", "start_talking");
-                emotiva.trigger("mouth", "tween_start");
-                emotiva.trigger("base", "tween_start");
-                move_char(&mut emotiva);
-                //emotiva.trigger("mouth", "idle_chat");
-                //emotiva.set_image("base", "hello");
+                emotiva.tween_start("eyes");
+                emotiva.tween_start("mouth");
+                emotiva.tween_start("base");
             } else {
                 println!("stop!");
                 emotiva.trigger("eyes", "stop_blinking");
-                emotiva.trigger("eyes", "tween_stop");
                 emotiva.trigger("mouth", "stop_talking");
-                emotiva.trigger("base", "tween_stop");
-                reverse_char(&mut emotiva);
-                //emotiva.reset_image("base");
+                emotiva.tween_stop("eyes");
+                emotiva.tween_stop("base");
             }
 
             toggled = !toggled;
