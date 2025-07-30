eab2eddff29086ed144d8d5f0b1387ddd13ff3f2
Luis Araujo <fureai.studio@gmail.com>
2025-07-30 03:23:58 -0400

    fix(examples): fix fuurin example
    - Added new format in .ron file
    - Added test dojo background
    
    Signed-off-by: Luis Araujo <fureai.studio@gmail.com>

--- examples/fuurin.rs
+++ examples/fuurin.rs
@@ -2,8 +2,20 @@ use emotiva::quad::EmotivaQuad;
 
 use macroquad::prelude::*;
 
-#[macroquad::main("Emotiva Viewer")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Testing Dojo".to_string(),
+        window_width: 1536,
+        window_height: 1024,
+        fullscreen: false,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    let texture: Texture2D = load_texture("test_data/dojo.png").await.unwrap();
+
     let mut emotiva = EmotivaQuad::load("test_data/example_fuurin.emotiva.ron", "test_data").await;
 
     let mut elapsed = 0.0;
@@ -17,17 +29,31 @@ async fn main() {
         elapsed += delta;
         if elapsed >= 10.0 {
             if toggled {
-                emotiva.trigger("base", "tween_start_easing");
+                emotiva.tween_start_easing("base");
             } else {
-                emotiva.trigger("base", "tween_stop_easing");
+                emotiva.tween_stop_easing("base");
             }
             toggled = !toggled;
             elapsed = 0.0;
         }
 
         emotiva.update(delta);
+        draw_dojo(texture.clone());
         emotiva.draw();
 
         next_frame().await;
     }
+}
+
+fn draw_dojo(texture: Texture2D) {
+    draw_texture_ex(
+        &texture,
+        0.0,
+        0.0,
+        WHITE,
+        DrawTextureParams {
+            dest_size: Some(Vec2::new(texture.width(), texture.height())),
+            ..Default::default()
+        },
+    );
 }
